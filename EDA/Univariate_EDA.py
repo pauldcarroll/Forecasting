d0c48728e7b03a30dbeff072e2d658a6fe5dc5c1
Paul Carroll <97230649+pauliecarroll@users.noreply.github.com>
2022-09-28 11:33:40 +0100

    Create Univariate_EDA.py

---
+++ EDA/Univariate_EDA.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+""" EDA repo to prepare your data for univariate testing """
+
+import numpy as np
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('../../data/weather.csv')
+df['date'] = pd.to_datetime(df[['Day','Month','Year']])
+df.set_index('date',inplace=True, drop=True)
+
+rename_map = {
+   
+    'Total Precipitation (low resolution) daily sum [sfc]':    'Rainfall',
+    'Total Cloud Cover daily mean [sfc]':'Cloudcover',
+   'Sunshine Duration daily sum [sfc]': 'Sunshine',
+    'Temperature daily mean [2 m above gnd]':'Temperature',
+    'Temperature daily max [2 m above gnd]':'Temperature_max',
+    'Relative Humidity daily mean [2 m above gnd]':'Humidity',
+    'Mean Sea Level Pressure daily mean [MSL]':'Pressure_mean',
+    'Shortwave Radiation daily sum [sfc]':'Radiation',
+     'Wind Speed daily max [900 mb]':'max_wind_speed',
+    'Wind Speed daily mean [900 mb]':'Windspeed_mean'
+}
+
+df.rename(columns=rename_map, inplace=True)
+
+""" Univariate preparation """
+
+def univariate_1(df):
+  dfU = df.iloc[0:,5:6]
+  return dfU
+
+
+def univariate_prep(data, n_in=1, n_out=1, dropnan=True):
+  n_vars = 1 if type(data is list else data.shape[1]
+  df = pd.DataFrame(data)
+  cols = list[]
+                     
+  for i in range(n_in, 0, -1):
+      cols.append(df.shift(i))
+  for i in range((0, n_out)):
+      cols.append(df.shift(-i))
+  msg = pd.concat(cols, axis=1)
+  if dropnan:
+      msg.dropna(inplace=True)
+  return msg.values
+                     
+  
+def walk_forward(data, n_test):
+    predictions = list()
+    train, test = train_test_split(data, n_test)
+    history = [x for x in train]
+
+    for i in range(len(test)):
+        x_test, y_test = test[i, :-1], test[i, 0]
+        yhat = xgboost_forecast(history, x_test)
+        predictions.append(yhat)
+        history.append(test[i])
+        print('>expected=%.1f, predicted=%.1f' % (y_test, yhat))
+#Input: %s, 
+    mae = mean_absolute_error(test[:, -1], predictions)
+    rmse = sqrt(mean_squared_error(test[:,-1],predictions))
+    
+    return mae, rmse,test[:, -1], predictions
+
+
+                
+                     
+ 
