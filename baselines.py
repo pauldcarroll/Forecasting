3c461267ea17ad117d7ff95c71c25feedf1dc9c5
Paul Carroll <97230649+pauliecarroll@users.noreply.github.com>
2022-09-30 10:57:09 +0100

    Update baselines.py

--- baselines.py
+++ baselines.py
@@ -17,13 +17,15 @@ statistics.mean(df.x)
 # e.g. the most recent 6 datapoints will have most relevance to a rising trend dataset of 100 long, than the first 6. 
 
 # Where x is the length of the mean to calculate, sliding window mean function.
-def most_recent_mean(df, x):
-    a = len(df)
+def most_recent_mean(df.z, x):
+    a = len(df.z)
     b = a-x
-    y = sum(df.iloc[b:a])
+    y = sum(df.z.iloc[b:a])
     return y/x
   
   
-# 3. Run a basic linear regression model on your dataset. 
+# 3. Run a basic linear regression model on your dataset. Note this may be your best performing model for a small dataset.
 
-from sklearn.linear_model import LinearRegression
+# view the Linear Regression .py file
+
+
